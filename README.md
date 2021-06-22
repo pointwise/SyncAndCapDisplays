b047efcb37be94d724326acb55ee4a119f64b10d
sarahhope-swaim <85300079+sarahhope-swaim@users.noreply.github.com>
2021-06-22 08:24:17 -0500

    Apply Cadence Design Systems License/Copyright/Disclaimer
    Pointwise is now Cadence Design Systems, Inc.  All files except those explicitly covered by their own license are provided as-is, as per the warranty disclaimer in the README and in all applicable files.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # SyncAndCapDisplays
+Copyright 2021 Cadence Design Systems, Inc. All rights reserved worldwide.
 
 This script can be used to sync the display of two simultaneously running instances of Pointwise, capturing the display of both and compositing them into a single image.
+
+
+## Disclaimer
+This file is licensed under the Cadence Public License Version 1.0 (the "License"), a copy of which is found in the LICENSE file, and is distributed "AS IS." 
+TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW, CADENCE DISCLAIMS ALL WARRANTIES AND IN NO EVENT SHALL BE LIABLE TO ANY PARTY FOR ANY DAMAGES ARISING OUT OF OR RELATING TO USE OF THIS FILE. 
+Please see the License for the full text of applicable terms.
--- sync_and_cap_displays.py
+++ sync_and_cap_displays.py
@@ -1,3 +1,14 @@
+#############################################################################
+#
+# (C) 2021 Cadence Design Systems, Inc. All rights reserved worldwide.
+#
+# This sample script is not supported by Cadence Design Systems, Inc.
+# It is provided freely for demonstration purposes only.
+# SEE THE WARRANTY DISCLAIMER AT THE BOTTOM OF THIS FILE.
+#
+#############################################################################
+
+
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
@@ -316,3 +327,15 @@ root = Tk()
 root.title("Sync And Cap Displays")
 app = Application(master=root)
 app.mainloop()
+
+
+#############################################################################
+#
+# This file is licensed under the Cadence Public License Version 1.0 (the
+# "License"), a copy of which is found in the included file named "LICENSE",
+# and is distributed "AS IS." TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE
+# LAW, CADENCE DISCLAIMS ALL WARRANTIES AND IN NO EVENT SHALL BE LIABLE TO
+# ANY PARTY FOR ANY DAMAGES ARISING OUT OF OR RELATING TO USE OF THIS FILE.
+# Please see the License for the full text of applicable terms.
+#
+#############################################################################
